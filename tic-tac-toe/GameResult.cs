523d136a816aea8a4b16f02adc97bb771c62881b
thientn20 <thientn20@uef.edu.vn>
2023-09-27 01:52:06 +0700

    AI-project

---
+++ tic-tac-toe/GameResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tic_tac_toe
+{
+    public class GameResult
+    {
+        public Player Winner {  get; set; }
+        public WinInfo WinInfo {  get; set; }
+    }
+}
